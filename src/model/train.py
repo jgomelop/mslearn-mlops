cfebfe0cf2b18aa0f820112a317a7c3ecfbebcc3
Juan Pablo Gomez Lopez <jpgomezl@sura.com.co>
2025-12-24 19:14:55 -0500

    Resolving TODOs

--- src/model/train.py
+++ src/model/train.py
@@ -1,18 +1,14 @@
-# Import libraries
-
 import argparse
 import glob
 import os
-
 import pandas as pd
-
+import mlflow
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -21,7 +17,7 @@ def main(args):
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    train_model(args.reg_rate, X_train, y_train)
 
 
 def get_csvs_df(path):
@@ -33,10 +29,12 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    # From notebook train-classification-model.ipynb
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
-
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
+def train_model(reg_rate, X_train, y_train):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
@@ -59,16 +57,9 @@ def parse_args():
 
 # run script
 if __name__ == "__main__":
-    # add space in logs
-    print("\n\n")
-    print("*" * 60)
-
     # parse args
     args = parse_args()
 
     # run main function
     main(args)
 
-    # add space in logs
-    print("*" * 60)
-    print("\n\n")
