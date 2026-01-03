a451599e04b6642221353be2d24e0440198c3b38
Juan Pablo Gomez Lopez <jpgomezl@sura.com.co>
2026-01-02 23:35:43 -0500

    git commit -m "Add Azure ML training pipeline and GitHub Actions workflow"

--- src/model/train.py
+++ src/model/train.py
@@ -17,7 +17,7 @@ def main(args):
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, y_train)
+    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
 def get_csvs_df(path):
@@ -34,10 +34,10 @@ def split_data(df):
     X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
-def train_model(reg_rate, X_train, y_train):
+def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
+    
 
 def parse_args():
     # setup arg parser
